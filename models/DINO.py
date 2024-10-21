31ea7216916cb182939d4284413e8b8c72d5cf4e
cykim8811 <cykim8811@snu.ac.kr>
2024-10-21 11:25:36 +0000

    fix dino again

--- models/DINO.py
+++ models/DINO.py
@@ -16,14 +16,14 @@ class DINO:
         alpha = alpha.T[np.newaxis, :, :]
         qm = qm[:, :, np.newaxis]
 
-        n = np.pow(alpha, qm).prod(axis=1)  # ndarray[문항 id, 학생 id] -> 0 or 1 (해당 학생이 해당 문항을 위한 인지요소를 모두 가지고 있는지 여부)
+        n = (1 - np.pow(alpha, qm)).prod(axis=1)  # ndarray[문항 id, 학생 id] -> 0 or 1 (해당 학생이 해당 문항을 위한 인지요소를 모두 가지고 있는지 여부)
 
         w = 1 - n
 
         s = self.slip[:, np.newaxis]
         g = self.guess[:, np.newaxis]
 
-        return np.pow((1 - s), 1 - w) * np.pow(g, w)    # ndarray[문항 id, 학생 id] -> 0~1 (해당 학생이 해당 문항을 맞출 확률)
+        return np.pow((1 - s), w) * np.pow(g, 1 - w)    # ndarray[문항 id, 학생 id] -> 0~1 (해당 학생이 해당 문항을 맞출 확률)
 
     def L(self, alpha, qm, x):
         # x: ndarray[학생 id, 문항 id] -> 0 or 1 (해당 학생이 해당 문항을 맞췄는지 여부)
